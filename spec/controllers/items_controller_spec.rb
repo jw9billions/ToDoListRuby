188dbd418177bcf2b524ca09e4fd9429d957b646
Jane Wang <jw9billions@gmail.com>
2017-11-07 19:07:36 -0800

    add post create in items_controller_spec.rb

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
 
+  before(:each) do
+    @my_user = FactoryGirl.create(:user)
+    sign_in @my_user
+  end
+
+  let(:item) { create(:item, user: @my_user) }
+  let(:item2) { create(:item, user: @my_user) }
+
+  describe "POST create" do
+    it "increases the number of Items by 1" do
+      expect { post :create, user_id: @my_user.id, item: {name: "test name"}}.to change(Item,:count).by(1)
+    end
+  end
+
   describe "GET #new" do
     it "returns http success" do
       get :new
